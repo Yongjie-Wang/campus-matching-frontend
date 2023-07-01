671ab91c1f85956d623e07e89f6eaa8b4eb3e1a8
Yongjie-Wang <3033784564@qq.com>
2023-07-01 13:54:42 +0000

    Update myAxios.ts

--- src/plugins/myAxios.ts
+++ src/plugins/myAxios.ts
@@ -3,7 +3,7 @@ import axios, {AxiosInstance} from "axios";
 const isDev = process.env.NODE_ENV === 'development';
 
 const myAxios: AxiosInstance = axios.create({
-    baseURL: isDev ? 'http://localhost:8081/api' : 'http://124.221.242.250:8081/api',
+    baseURL: isDev ? 'http://localhost:8081/api' : 'xxx',
 });
 
 myAxios.defaults.withCredentials = true; // 配置为true
